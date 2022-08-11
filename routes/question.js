0f5b209a80269742a23cf280e2c8782986ebd368
Benjamin Johnson <ben332004@gmail.com>
2022-08-11 14:39:55 -0700

    Route to get specific question

--- routes/question.js
+++ routes/question.js
@@ -1,11 +1,22 @@
 const router = require("express").Router();
 const passport = require("passport");
-const { getQuestionsUnderCategory } = require("../db/services/question");
+const { findOptionById } = require("../db/services/option");
+const {
+  getQuestionsUnderCategory,
+  findQuestionById,
+} = require("../db/services/question");
 
 router.get("/category/:categoryId", async (req, res) => {
-    let questions = await getQuestionsUnderCategory(req.params.categoryId);
-    res.json({questions : questions/*, answeredQuestions : req.user.answeredQuestions*/}); 
- });
+  let questions = await getQuestionsUnderCategory(req.params.categoryId);
+  res.json({
+    questions: questions /*, answeredQuestions : req.user.answeredQuestions*/,
+  });
+});
 
+router.get("/questionId", async (req, res) => {
+  const question = await findQuestionById(req.params.questionId);
+  const options = question.options.map(optionId => await findOptionById(optionId))
+  res.json({ question: {...question, options : options} });
+});
 
 module.exports = router;
